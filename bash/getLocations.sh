e8929538c2c8346f4fc6194725a51892a489fbc1
Tom Davidson <tom.davidson@ugent.be>
2012-11-15 16:32:00 +0100

    Improved output

--- bash/getLocations.sh
+++ bash/getLocations.sh
@@ -3,10 +3,7 @@ nameFile=$2
 locFile=$3
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $scriptdir
 sourcedir=${scriptdir:0:${#scriptdir}-4}
-echo $sourcedir
 export RAPIDSMITH_PATH=$sourcedir/third_party/rapidSmith
-#export RAPIDSMITH_PATH=~/recomp/tools/rapidSmith/trunk/rapidSmith-0.5.1/
 java -jar $sourcedir/third_party/rapidSmith/ExtractInfo.jar $xdlFile $nameFile $locFile
 
