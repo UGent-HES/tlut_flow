5d876118de1d60897fe8d4b2c2f8b16f6171ad2d
Karel Heyse <karel.heyse@ugent.be>
2013-01-04 13:44:05 +0100

    improved tests

--- tests/virtex2pro_test.sh
+++ tests/virtex2pro_test.sh
@@ -19,27 +19,32 @@ fi
 
 function testCase {
     echo Testing $1
+    echo Testing $1 >> work/virtex2pro_output.log
+    oldPWD=$PWD
     cd ../examples/$1/pcores/$2/design/
-    generateTMAPMake.py >/dev/null
+    generateTMAPMake.py >> $oldPWD/work/virtex2pro_output.log
     cd - >/dev/null
     cd ../examples/$1
     make -f custom.make clean >/dev/null
     rm -f received.txt
     cat > received.txt < /dev/ttyS0&
-    stty 580:5:cbd:8a3b:3:1c:7f:15:4:0:1:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0 -F /dev/ttyS0 #stty -g /dev/ttyS0
-    make -f custom.make download >/dev/null
     PID=$!
+    stty 580:5:cbd:8a3b:3:1c:7f:15:4:0:1:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0 -F /dev/ttyS0 #stty -g /dev/ttyS0
+    make -f custom.make download >> $oldPWD/work/virtex2pro_output.log
     sleep 10
     kill $PID
     #wait $PID 2>/dev/null
     if diff received.txt received_expected.txt >/dev/null ; then
-      echo $1 succeeded
+      echo "$1 succeeded"
     else
-      echo $1 failed
+      echo "$1 failed"
+      echo "Log file: work/virtex2pro_output.log"
       exit 1
     fi
     cd - >/dev/null
 }
+
+rm -f work/virtex2pro_output.log
 
 testCase "xorExample/xps" "opb_xor_v1_00_a"
 testCase "xorExample/xps_mi" "opb_xor_v1_00_a"
