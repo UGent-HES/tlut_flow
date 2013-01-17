10e65807ee310772bce511f92332ea6c33c5bcc6
Karel Heyse <karel.heyse@ugent.be>
2013-01-17 11:34:51 +0100

    missing file virtex 5 project

--- examples/xorExample/xpsV13/workspace/xorTest_0/Debug/subdir.mk
+++ examples/xorExample/xpsV13/workspace/xorTest_0/Debug/subdir.mk
@@ -4,9 +4,9 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-/home/kheyse/private/clone/examples/xorExample/xpsV13/swReconfiguration/exorw32.c \
-/home/kheyse/private/clone/examples/xorExample/xpsV13/swReconfiguration/locations.c \
-/home/kheyse/private/clone/examples/xorExample/xpsV13/swReconfiguration/testReconfiguration.c 
+../../../swReconfiguration/exorw32.c \
+../../../swReconfiguration/locations.c \
+../../../swReconfiguration/testReconfiguration.c 
 
 OBJS += \
 ./exorw32.o \
@@ -20,21 +20,21 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-exorw32.o: /home/kheyse/private/clone/examples/xorExample/xpsV13/swReconfiguration/exorw32.c
+exorw32.o: ../../../swReconfiguration/exorw32.c
 	@echo Building file: $<
 	@echo Invoking: PowerPC gcc compiler
 	powerpc-eabi-gcc -Wall -O0 -g3 -c -fmessage-length=0 -I../../xorTest_bsp_0/ppc440_0/include -mcpu=440 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo Finished building: $<
 	@echo ' '
 
-locations.o: /home/kheyse/private/clone/examples/xorExample/xpsV13/swReconfiguration/locations.c
+locations.o: ../../../swReconfiguration/locations.c
 	@echo Building file: $<
 	@echo Invoking: PowerPC gcc compiler
 	powerpc-eabi-gcc -Wall -O0 -g3 -c -fmessage-length=0 -I../../xorTest_bsp_0/ppc440_0/include -mcpu=440 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo Finished building: $<
 	@echo ' '
 
-testReconfiguration.o: /home/kheyse/private/clone/examples/xorExample/xpsV13/swReconfiguration/testReconfiguration.c
+testReconfiguration.o: ../../../swReconfiguration/testReconfiguration.c
 	@echo Building file: $<
 	@echo Invoking: PowerPC gcc compiler
 	powerpc-eabi-gcc -Wall -O0 -g3 -c -fmessage-length=0 -I../../xorTest_bsp_0/ppc440_0/include -mcpu=440 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
