bb74a6656f67a0c786671c1f5fab229fef9bce5a
Karel Heyse <karel.heyse@ugent.be>
2013-03-01 13:27:41 +0000

    microblaze example added

---
+++ examples/xorExample/xpsV13_blaze/drivers/plb_xor_v1_00_a/src/plb_xor.c
@@ -0,0 +1,14 @@
+/*****************************************************************************
+* Filename:          /home/todavids/private/git/tlut/examples/xorExample/xpsV13_blaze/drivers/plb_xor_v1_00_a/src/plb_xor.c
+* Version:           1.00.a
+* Description:       plb_xor Driver Source File
+* Date:              Tue Feb 26 15:31:48 2013 (by Create and Import Peripheral Wizard)
+*****************************************************************************/
+
+
+/***************************** Include Files *******************************/
+
+#include "plb_xor.h"
+
+/************************** Function Definitions ***************************/
+
---
+++ examples/xorExample/xpsV13_blaze/workspace/testReconf/Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/platform.c 
+
+LD_SRCS += \
+../src/lscript.ld 
+
+OBJS += \
+./src/platform.o 
+
+C_DEPS += \
+./src/platform.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo Building file: $<
+	@echo Invoking: MicroBlaze gcc compiler
+	mb-gcc -Wall -O0 -g3 -c -fmessage-length=0 -I../../hello_world_bsp_0/microblaze_0/include -mxl-barrel-shift -mxl-pattern-compare -mcpu=v8.20.b -mno-xl-soft-mul -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo Finished building: $<
+	@echo ' '
+
+
