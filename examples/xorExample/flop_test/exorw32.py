6f225844e99dd8dea0e801ff3db6a72efc6c7f4d
Karel Heyse <karel.heyse@ugent.be>
2013-07-15 14:55:20 +0200

    xst synthesis test fix

--- examples/xorExample/flop_test/exorw32.py
+++ examples/xorExample/flop_test/exorw32.py
@@ -5,7 +5,7 @@ from fast_tlutmap import run
 
 def main():
     run('exorw32.vhd', performCheck=True, verboseFlag=False, generateImplementationFilesFlag=True, virtexFamily='virtex5')
-    assert os.system('xst -ifn work/exorw32/exorw32-simpletmap.vhd')
+    assert os.system('xst -ifn exorw32_xst.scr')
 
 if __name__=="__main__":
     main()
