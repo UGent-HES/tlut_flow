4763a8b6d9c5c354dd21b51f73025db42fb7aecc
Karel Heyse <karel.heyse@ugent.be>
2014-01-21 14:12:26 +0100

    add tconmap to fast_tlutmap script

--- python/src/fast_tlutmap.py
+++ python/src/fast_tlutmap.py
@@ -128,6 +128,14 @@ def run(module, submodules=[], K=4, virtexFamily=None, performCheck=True, genera
         simpleTMapper(baseName, synthesizedFileName, parameterFileName, K, performCheck, generateImplementationFilesFlag, module, verboseFlag, targetDepth) #, [ '--notlut'])
     print collumnize(['Luts (TLUTS)','depth','check'],colwidth)
     print collumnize([str(numLuts)+' ('+str(numTLUTs)+')',depth,check],colwidth)
+
+    # Unleash TCON mapper
+    print "Stage: TCON mapper"
+    numLuts, numTLUTs, depth, avDup, origAnds, paramAnds, check = \
+        simpleTMapper(baseName, synthesizedFileName, parameterFileName, K, performCheck, generateImplementationFilesFlag, module, verboseFlag, targetDepth, ['--tcon', '--allowDepthIncrease'])
+    print collumnize(['Luts (TLUTS)','depth','check'],colwidth)
+    print collumnize([str(numLuts)+' ('+str(numTLUTs)+')',depth,check],colwidth)
+ 
     
     # Print C-files
     if generateImplementationFilesFlag:
