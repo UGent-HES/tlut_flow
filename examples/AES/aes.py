b2a3108608395e7a2d728f81eebd3d40f81dea78
Karel Heyse <karel.heyse@ugent.be>
2014-01-29 15:58:37 +0100

    change aes script to use synthesizedFileName option in fast_run script

--- examples/AES/aes.py
+++ examples/AES/aes.py
@@ -1,41 +1,9 @@
 #!/usr/bin/env python
 
-import os
-import shutil
-from mapping import * 
-from fast_tlutmap import collumnize
+from fast_tlutmap import run
 
-colwidth=16
 def main():
-    K = 4
-    performCheck = True
-    verboseFlag = False
-    setMaxMemory(2048)
-    
-    baseName = "k_AES"
-    aagFileName = baseName+".aag"
-    parameterFileName = baseName+".par"
-    
-    print "Stage: Creating work directory and copying design"
-    os.system('mkdir -p work')
-    shutil.copy(aagFileName, 'work')
-    shutil.copy(parameterFileName, 'work')
-    shutil.copy(os.environ['TLUTFLOW_PATH']+'/third_party/etc/abc.rc','work')
-    
-    os.chdir('work')
-    
-    # Unleash TLUTMAP
-    print "Stage: TLUTMAP"
-    numLuts, numTLUTs, depth, avDup, origAnds, paramAnds, check = simpleTMapper(baseName, aagFileName, parameterFileName, K, performCheck, False, verboseFlag=verboseFlag)
-    print collumnize(['Luts (TLUTS)','depth','check'],colwidth)
-    print collumnize([str(numLuts)+' ('+str(numTLUTs)+')',depth,check],colwidth)
-    
-    # Run regular MAP
-    print "Stage: SimpleMAP"
-    numLuts, depth, check = simpleMapper(baseName, aagFileName, K, performCheck, verboseFlag)
-    print collumnize(['Luts','depth','check'],colwidth)
-    print collumnize([numLuts,depth,check],colwidth)
-    
+    run('k_AES.aag', [], K=4, performCheck=True, verboseFlag=False, synthesizedFileName='k_AES.aag', parameterFileName='k_AES.par')
 
 if __name__=="__main__":
     main()
