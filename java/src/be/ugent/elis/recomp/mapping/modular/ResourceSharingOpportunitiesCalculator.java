97bb1fa525307feb7c6af6ae8db14b552144ce35
Karel Heyse <karel.heyse@ugent.be>
2014-01-30 16:03:45 +0100

    extract inner class ActivationSet

--- java/src/be/ugent/elis/recomp/mapping/modular/ResourceSharingOpportunitiesCalculator.java
+++ java/src/be/ugent/elis/recomp/mapping/modular/ResourceSharingOpportunitiesCalculator.java
@@ -73,25 +73,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
 
 import net.sf.javabdd.BDD;
 import net.sf.javabdd.BDDFactory;
-import net.sf.javabdd.BuDDyFactory;
 
-import be.ugent.elis.recomp.aig.AIG;
 import be.ugent.elis.recomp.mapping.tmapSimple.ParameterMarker;
-import be.ugent.elis.recomp.mapping.utils.Edge;
 import be.ugent.elis.recomp.mapping.utils.MappingAIG;
 import be.ugent.elis.recomp.mapping.utils.Node;
 import be.ugent.elis.recomp.synthesis.BDDFactorySingleton;
 
 public class ResourceSharingOpportunitiesCalculator {
-	
-	boolean short_activation_function = true;
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -105,98 +98,13 @@ public class ResourceSharingOpportunitiesCalculator {
     }
 	
 	
-	class ActivationSet {
-		final private MappingAIG aig;
-		final private BDD activationFunction;
-		private Set<Node> nodes;
-		private Set<ActivationSet> sharingOpportunities;
-		
-		ActivationSet(MappingAIG aig, BDD activationFunction) {
-			this.aig = aig;
-			this.activationFunction = activationFunction;
-			nodes = new HashSet<Node>();
-			sharingOpportunities = new HashSet<ActivationSet>();
-		}
-		public boolean disjunctWithSet(ActivationSet otherSet) {
-			BDD bdd = getActivationFunction().and(otherSet.getActivationFunction());
-			boolean res = bdd.isZero();
-			bdd.free();
-			return res;
-		}
-		public BDD getActivationFunction() {
-			return activationFunction;
-		}
-		public void addNode(Node node) {
-			nodes.add(node);
-		}
-		public int getNumAndNodes() {
-			int num_and_nodes = 0;
-			for(Node node : getNodes())
-				num_and_nodes += node.isGate() ? 1 : 0;
-			return num_and_nodes;
-		}
-		public Set<Node> getNodes() {
-			return nodes;
-		}
-		public Set<ActivationSet> getSharingOpportunities() {
-			return sharingOpportunities;
-		}
-		public void setSharingOpportunities(Set<ActivationSet> sharingOpportunities) {
-			this.sharingOpportunities = sharingOpportunities;
-		}
-		public void addSharingOpportunity(ActivationSet sharingOpportunity) {
-			this.sharingOpportunities.add(sharingOpportunity);
-		}
-		public boolean canShareWith(ActivationSet set) {
-			return getSharingOpportunities().contains(set);
-		}
-		public boolean sanityCheck() {
-			if(getSharingOpportunities() != null) {
-				for(ActivationSet child : getSharingOpportunities())
-					if(!disjunctWithSet(child)) return false;
-			}
-			return true;
-		}
-		public String toString() {
-			StringBuilder sb = new StringBuilder();
-			sb.append("ActivationSet(");
-			sb.append("activation_function{");
-			if(short_activation_function)
-				sb.append(getActivationFunction().hashCode());
-			else
-				sb.append(getActivationFunction().toString());
-			sb.append("},share_with{");
-			for(ActivationSet set : getSharingOpportunities()) {
-				if(short_activation_function)
-					sb.append(set.getActivationFunction().hashCode());
-				else
-					sb.append(set.getActivationFunction().toString());
-				sb.append(',');
-			}
-			sb.append("},num_and_nodes{"+getNumAndNodes());
-			sb.append("},num_nodes{");
-			sb.append(getNodes().size());
-//			sb.append("},nodes{");
-//			for(Node node : getNodes()) {
-//				sb.append(node.getName());
-//				sb.append(',');
-//			}
-			sb.append("},bdd_vars{");
-			int varProfile[] = getActivationFunction().varProfile();
-			for(int id = 0; id < varProfile.length; id++)
-				if(varProfile[id] != 0)
-					sb.append(""+aig.getBDDidMapping().getNode(id).getName()+",");
-			sb.append("})");
-			return sb.toString();
-		}
-		public MappingAIG getAIG() {
-			return aig;
-		}
+	private BDDFactory B;
+	private Map<BDD,ActivationSet> activationSets;
+	
+	public Map<BDD, ActivationSet> getActivationSets() {
+		return activationSets;
 	}
 
-	BDDFactory B;
-	Map<BDD,ActivationSet> activationSets;
-	
 	public ResourceSharingOpportunitiesCalculator() {
 		activationSets = new HashMap<BDD,ActivationSet>();
 	}
@@ -223,6 +131,13 @@ public class ResourceSharingOpportunitiesCalculator {
 			System.exit(1);
 		}
 		System.out.println("Total num nodes: "+allNodes.size());
+	}
+	
+	public int totalNumAndNodes() {
+		int num_nodes = 0;
+		for(ActivationSet activationset : activationSets.values())
+			num_nodes += activationset.getNumAndNodes();
+		return num_nodes;
 	}
 	
 	public void run(MappingAIG aig) {
