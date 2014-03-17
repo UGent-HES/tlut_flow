a84f7f5f7d3fc364c624d01332aea6cdff52c9c4
Karel Heyse <karel.heyse@ugent.be>
2014-03-17 14:25:58 +0100

    updated modular AES/3DES exp results

--- examples/modular/modules_AES_3DES/modules.py
+++ examples/modular/modules_AES_3DES/modules.py
@@ -5,7 +5,7 @@ from fast_tconmap import run, setMaxMemory
 
 def main():
     setMaxMemory(2048)
-    run('modules_exp.vhd', glob.glob('*/*.vhd'), K=4, performCheck=False, verboseFlag=True, resynthesizeFlag=True)
+    run('modules_exp.vhd', glob.glob('*/*.vhd'), K=4, performCheck=False, verboseFlag=False, resynthesizeFlag=True)
 
 if __name__=="__main__":
     main()
