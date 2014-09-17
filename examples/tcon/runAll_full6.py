6268564c1e5f8b5f3ba5b7bc9670b13a408fa210
Karel Heyse <karel.heyse@ugent.be>
2014-09-17 16:08:41 +0200

    results tcon experiments set

---
+++ examples/tcon/runAll_full6.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import glob
+from fast_tconmap import run, setMaxMemory
+
+
+K = 6
+
+#setMaxMemory(5000)
+setMaxMemory(1024)
+
+# os.chdir('barrelshift')
+# ./barrelshift.py
+# os.chdir('..')
+
+os.chdir('crossbar')
+module = "cross16-sweep.blif"
+print module
+run(module, 
+    K=K, 
+    performCheck=True, 
+    verboseFlag=False,
+    synthesizedFileName=module, 
+    parameterFileName="parameters_cross16.par")
+os.chdir('..')
+
+# os.chdir('crossbar')
+# module = "switch16x16-sweep-sclean.blif"
+# print module
+# run(module, 
+#     K=K, 
+#     performCheck=False, 
+#     verboseFlag=False,
+#     synthesizedFileName=module, 
+#     parameterFileName="parameters_clos16.par", 
+#     extraArgs=['--nolutstruct'])
+# os.chdir('..')
+
+# os.chdir('mux')
+# module = "mux3.blif"
+# print 'mux'
+# run(module, 
+#     K=K, 
+#     performCheck=True, 
+#     verboseFlag=False,
+#     resynthesizeFlag=True,
+#     synthesizedFileName=module, 
+#     parameterFileName="parameters_mux.par",
+#     extraArgs=['--nolutstruct'])
+# os.chdir('..')
+
+os.chdir('tlc2')
+print 'tlc2'
+module = "tlc2-sweep.blif"
+run(
+    module, 
+    K=K, 
+    performCheck=False, 
+    verboseFlag=False,
+    synthesizedFileName=module, 
+    parameterFileName="parameters_ps.par",
+    extraArgs=['--nolutstruct'])
+os.chdir('..')
+
+os.chdir('tlc4')
+print 'tlc4'
+module = "tlc4-sweep.blif"
+run(module, 
+    K=K, 
+    performCheck=True, 
+    verboseFlag=False,
+    synthesizedFileName=module, 
+    parameterFileName="parameters_ps.par")
+os.chdir('..')
+
+# os.chdir('regEx')
+# ./combined_blocks.py
+# os.chdir('..')
+
+os.chdir('regExPE')
+print "regex"
+setMaxMemory(5000)
+run("design/grid_tile.vhd", 
+    glob.glob("design/*.vhd"), 
+    K=K, 
+    performCheck=True, 
+    verboseFlag=False,
+    resynthesizeFlag=True)
+os.chdir('..')
